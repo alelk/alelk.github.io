b7e6279906b6b6e5be1bbec9b17f21071329adf5
Alex Elkin <alelkdev@gmail.com>
2017-11-28 09:25:48 +0300

    New skill descriptions have been added.

--- src/data/skills.js
+++ src/data/skills.js
@@ -95,6 +95,25 @@ const skills = [
                     }
                 ]
             }, {
+                titleId: "skills.java.app-servers",
+                defaultTitle: "Application Servers",
+                level: 50,
+                skills: [
+                    {
+                        titleId: "skills.java.app-servers.wildfly",
+                        defaultTitle: "WildFly",
+                        descriptionId: "skills.java.app-servers.wildfly_description",
+                        level: 50,
+                        href: "http://wildfly.org"
+                    }, {
+                        titleId: "skills.java.app-servers.tomcat",
+                        defaultTitle: "Apache Tomcat",
+                        descriptionId: "skills.java.app-servers.tomcat_description",
+                        level: 40,
+                        href: "http://tomcat.apache.org/"
+                    }
+                ]
+            }, {
                 titleId: "skills.java.build-tools",
                 commentId: "skills.java.build-tools_comment",
                 defaultTitle: "Build Automation Tools",
@@ -149,31 +168,37 @@ const skills = [
         skills: [
             {
                 titleId: "skills.web.react",
+                descriptionId: "skills.web.react_description",
                 defaultTitle: "React",
                 level: 90,
                 href: "https://facebook.github.io/react"
             }, {
                 titleId: "skills.web.redux",
+                descriptionId: "skills.web.redux_description",
                 defaultTitle: "Redux",
                 level: 80,
                 href: "http://redux.js.org"
             }, {
                 titleId: "skills.web.html",
+                descriptionId: "skills.web.html_description",
                 defaultTitle: "HTML",
                 level: 70,
                 href: "https://www.w3schools.com/html/"
             }, {
                 titleId: "skills.web.css",
+                descriptionId: "skills.web.css_description",
                 defaultTitle: "CSS3",
                 level: 80,
                 href: "http://www.w3schools.com/css/"
             }, {
                 titleId: "skills.web.js",
+                descriptionId: "skills.web.js_description",
                 defaultTitle: "JavaScript",
                 level: 60,
                 href: "http://www.w3schools.com/js/default.asp"
             }, {
                 titleId: "skills.web.es6",
+                descriptionId: "skills.web.es6_description",
                 defaultTitle: "ES6",
                 level: 60,
                 href: "https://es6.io"
@@ -187,61 +212,73 @@ const skills = [
                 skills: [
                     {
                         titleId: "skills.web.libraries.rxjs",
+                        descriptionId: "skills.web.libraries.rxjs_description",
                         defaultTitle: "RxJS",
                         level: 30,
                         href: "http://reactivex.io/rxjs/"
                     }, {
                         titleId: "skills.web.libraries.semantic-ui",
+                        descriptionId: "skills.web.libraries.semantic-ui_description",
                         defaultTitle: "Semantic UI React",
                         level: 40,
                         href: "https://react.semantic-ui.com"
                     }, {
                         titleId: "skills.web.libraries.material-ui",
+                        descriptionId: "skills.web.libraries.material-ui_description",
                         defaultTitle: "Material-UI",
                         level: 40,
                         href: "http://www.material-ui.com"
                     }, {
                         titleId: "skills.web.libraries.redux-thunk",
+                        descriptionId: "skills.web.libraries.redux-thunk_description",
                         defaultTitle: "Redux Thunk",
                         level: 50,
                         href: "https://github.com/gaearon/redux-thunk"
                     }, {
                         titleId: "skills.web.libraries.react-router",
+                        descriptionId: "skills.web.libraries.react-router_description",
                         defaultTitle: "React Router",
                         level: 50,
                         href: "https://github.com/ReactTraining/react-router"
                     }, {
                         titleId: "skills.web.libraries.react-intl",
+                        descriptionId: "skills.web.libraries.react-intl_description",
                         defaultTitle: "React Intl",
                         level: 50,
                         href: "https://github.com/yahoo/react-intl"
                     }, {
                         titleId: "skills.web.libraries.react-table",
+                        descriptionId: "skills.web.libraries.react-table_description",
                         defaultTitle: "React Table",
                         level: 80,
                         href: "https://react-table.js.org"
                     }, {
                         titleId: "skills.web.libraries.sockjs",
+                        descriptionId: "skills.web.libraries.sockjs_description",
                         defaultTitle: "SockJS",
                         level: 50,
                         href: "http://github.com/sockjs/sockjs-client"
                     }, {
                         titleId: "skills.web.libraries.stomp",
+                        descriptionId: "skills.web.libraries.stomp_description",
                         defaultTitle: "STOMP",
                         level: 50,
                         href: "http://jmesnil.net/stomp-websocket/doc"
                     }, {
                         titleId: "skills.web.libraries.jquery",
+                        descriptionId: "skills.web.libraries.jquery_description",
                         defaultTitle: "JQuery",
                         level: 30,
                         href: "https://jquery.com"
                     }, {
                         titleId: "skills.web.libraries.chartjs",
+                        descriptionId: "skills.web.libraries.chartjs_description",
                         defaultTitle: "Chart.js",
                         level: 60,
                         href: "http://www.chartjs.org"
                     }, {
                         titleId: "skills.web.libraries.normalizr",
+                        descriptionId: "skills.web.libraries.normalizr_description",
                         defaultTitle: "normalizr",
                         level: 70,
                         href: "http://github.com/paularmstrong/normalizr"
@@ -254,11 +291,13 @@ const skills = [
                 skills: [
                     {
                         titleId: "skills.web.project-tools.npm",
+                        descriptionId: "skills.web.project-tools.npm_description",
                         defaultTitle: "Node Package Manager (npm)",
                         level: 20,
                         href: "https://www.npmjs.com/"
                     }, {
                         titleId: "skills.web.project-tools.yarn",
+                        descriptionId: "skills.web.project-tools.yarn_description",
                         defaultTitle: "Yarn",
                         level: 30,
                         href: "https://yarnpkg.com"
@@ -269,21 +308,25 @@ const skills = [
     }, {
         titleId: "skills.android",
         commentId: "skills.android_comment",
+        descriptionId: "skills.android_description",
         defaultTitle: "Android Development",
         level: 50,
         skills: [
             {
                 titleId: "skills.android.dagger",
+                descriptionId: "skills.android.dagger_description",
                 defaultTitle: "Dagger 2",
                 level: 30,
                 href: "https://google.github.io/dagger"
             }, {
                 titleId: "skills.android.arch-components",
+                descriptionId: "skills.android.arch-components_description",
                 defaultTitle: "Android Architecture Components",
                 level: 30,
                 href: "https://developer.android.com/topic/libraries/architecture/index.html"
             }, {
                 titleId: "skills.android.data-binding",
+                descriptionId: "skills.android.data-binding_description",
                 defaultTitle: "Data Binding Library",
                 level: 40,
                 href: "https://developer.android.com/topic/libraries/data-binding/index.html"
@@ -291,6 +334,7 @@ const skills = [
         ]
     }, {
         titleId: "skills.c-sharp",
+        descriptionId: "skills.c-sharp_description",
         commentId: "skills.c-sharp_comment",
         defaultTitle: "C# Development",
         level: 40
@@ -301,21 +345,25 @@ const skills = [
         skills: [
             {
                 titleId: "skills.databases.sql",
+                descriptionId: "skills.databases.sql_description",
                 defaultTitle: "SQL",
                 level: 60,
                 href: "https://www.w3schools.com/sql"
             }, {
                 titleId: "skills.databases.sqlite",
+                descriptionId: "skills.databases.sqlite_description",
                 defaultTitle: "SQLite",
                 level: 90,
                 href: "https://www.sqlite.org"
             }, {
                 titleId: "skills.databases.postgresql",
+                descriptionId: "skills.databases.postgresql_description",
                 defaultTitle: "PostgreSQL",
                 level: 50,
                 href: "https://www.postgresql.org/"
             }, {
                 titleId: "skills.databases.mysql",
+                descriptionId: "skills.databases.mysql_description",
                 defaultTitle: "MySQL",
                 level: 30,
                 href: "https://www.mysql.com/"
@@ -328,11 +376,13 @@ const skills = [
         skills: [
             {
                 titleId: "skills.vcs.git",
+                descriptionId: "skills.vcs.git_description",
                 defaultTitle: "Git",
                 level: 90,
                 href: "https://git-scm.com"
             }, {
                 titleId: "skills.vcs.svn",
+                descriptionId: "skills.vcs.svn_description",
                 defaultTitle: "SVN",
                 level: 20,
                 href: "http://subversion.apache.org"
@@ -346,11 +396,13 @@ const skills = [
         skills: [
             {
                 titleId: "skills.os.linux",
+                descriptionId: "skills.os.linux_description",
                 defaultTitle: "Linux",
                 level: 60,
                 href: "https://www.ubuntu.com/"
             }, {
                 titleId: "skills.os.windows",
+                descriptionId: "skills.os.windows_description",
                 defaultTitle: "Windows",
                 level: 60,
                 href: "http://www.microsoft.com/en-us/windows/"
@@ -358,22 +410,26 @@ const skills = [
         ]
     }, {
         titleId: "skills.scada",
+        descriptionId: "skills.scada_description",
         commentId: "skills.scada_comment",
         defaultTitle: "SCADA Development",
         level: 40,
         skills: [
             {
                 titleId: "skills.scada.ms3",
+                descriptionId: "skills.scada.ms3_description",
                 defaultTitle: "Master SCADA 3.x",
                 level: 60,
                 href: "http://insat.ru"
             }, {
                 titleId: "skills.scada.ms4",
+                descriptionId: "skills.scada.ms4_description",
                 defaultTitle: "Master SCADA 4D",
                 level: 30,
                 href: "http://insat.ru"
             }, {
                 titleId: "skills.scada.universal-opc",
+                descriptionId: "skills.scada.universal-opc_description",
                 defaultTitle: "Universal Master OPC Server",
                 level: 90,
                 href: "http://insat.ru"
@@ -386,14 +442,17 @@ const skills = [
         skills: [
             {
                 titleId: "skills.other.cpp",
+                descriptionId: "skills.other.cpp_description",
                 defaultTitle: "C/C++",
                 level: 20
             }, {
                 titleId: "skills.other.perl",
+                descriptionId: "skills.other.perl_description",
                 defaultTitle: "Perl",
                 level: 40
             }, {
                 titleId: "skills.other.assembler",
+                descriptionId: "skills.other.assembler_description",
                 defaultTitle: "Assembler",
                 level: 10
             }
--- src/messages/enMessages.js
+++ src/messages/enMessages.js
@@ -56,6 +56,13 @@ const messages = {
     "web page generation with MVC software architecture.",
     "skills.java.libraries.jackson" : "Jackson",
     "skills.java.libraries.jackson_description" : "Jackson is a high-performance JSON processor for Java.",
+    "skills.java.app-servers" : "Application Servers",
+    "skills.java.app-servers.wildfly" : "WildFly",
+    "skills.java.app-servers.wildfly_description" : "WildFly is an application server authored by JBoss, now developed " +
+    "by Red Hat. WildFly is written in Java, and implements the Java Platform, Enterprise Edition specification.",
+    "skills.java.app-servers.tomcat" : "Apache Tomcat",
+    "skills.java.app-servers.tomcat_description" : "Apache Tomcat is an open source implementation of the Java Servlet, " +
+    "JavaServer Pages, Java Expression Language and Java WebSocket technologies.",
     "skills.java.build-tools" : "Build Automation Tools",
     "skills.java.build-tools_comment" : "Maven is my favorite. Also I familiar with Gradle because of Android.",
     "skills.java.build-tools.maven" : "Maven",
@@ -80,63 +87,145 @@ const messages = {
     "skills.web_comment" : "Thanks to React & Redux, I'm very interested in the web development. I'm not web expert " +
     "yet, but I'm on my way to this.",
     "skills.web.react" : "React",
+    "skills.web.react_description" : "React is a JavaScript library for building user interfaces. It is maintained " +
+    "by Facebook, Instagram and a community of individual developers and corporations.",
     "skills.web.redux" : "Redux",
+    "skills.web.redux_description" : "Redux is an open-source JavaScript library designed for managing application " +
+    "state. It is primarily used together with React or angular js for building user interfaces.",
     "skills.web.html" : "HTML",
+    "skills.web.html_description" : "Hypertext Markup Language is the standard markup language for creating web " +
+    "pages and web applications. With Cascading Style Sheets and JavaScript it forms a triad of cornerstone " +
+    "technologies for the World Wide Web.",
     "skills.web.css" : "CSS",
+    "skills.web.css_description" : "Cascading Style Sheets is a style sheet language used for describing the " +
+    "presentation of a document written in a markup language.",
     "skills.web.js" : "JavaScript",
+    "skills.web.js_description" : "JavaScript is a high-level, dynamic, weakly typed, prototype-based, " +
+    "multi-paradigm, and interpreted programming language.",
     "skills.web.es6" : "ES6",
+    "skills.web.es6_description" : "ES6 is a significant update to the JavaScript.",
     "skills.web.libraries": "Web Libraries",
     "skills.web.libraries.rxjs" : "RxJS",
+    "skills.web.libraries.rxjs_description" : "RxJS is a library for reactive programming using Observables, " +
+    "to make it easier to compose asynchronous or callback-based code.",
     "skills.web.libraries.semantic-ui" : "Semantic UI React",
+    "skills.web.libraries.semantic-ui_description" : "Semantic UI is an user interface framework. Semantic empowers " +
+    "designers and developers by creating a shared vocabulary for UI.",
     "skills.web.libraries.material-ui" : "Material-UI",
+    "skills.web.libraries.material-ui_description" : "Material-UI is a Google's material design UI components built with React.",
     "skills.web.libraries.redux-thunk" : "Redux Thunk",
+    "skills.web.libraries.redux-thunk_description" : "Redux Thunk is a middleware which allows you to write action " +
+    "creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action," +
+    " or to dispatch only if a certain condition is met.",
     "skills.web.libraries.react-router" : "React Router",
+    "skills.web.libraries.react-router_description" : "React Router is a collection of navigational components " +
+    "that compose declaratively with your application.",
     "skills.web.libraries.react-intl" : "React Intl",
+    "skills.web.libraries.react-intl_description" : "React Intl internationalize React apps. This library provides " +
+    "React components and an API to format dates, numbers, and strings, including pluralization and handling translations.",
     "skills.web.libraries.react-table" : "React Table",
+    "skills.web.libraries.react-table_description" : "A fast, lightweight, opinionated table and datagrid built on React.",
     "skills.web.libraries.sockjs" : "SockJS",
-    "skills.web.libraries.stomp" : "STOMP",
-    "skills.web.libraries.jquery" : "JQuery",
+    "skills.web.libraries.sockjs_description" : "SockJS is a browser JavaScript library that provides a " +
+    "WebSocket-like object. SockJS gives you a coherent, cross-browser, Javascript API which creates a low latency, " +
+    "full duplex, cross-domain communication channel between the browser and the web server.",
+    "skills.web.libraries.stomp" : "STOMP JS",
+    "skills.web.libraries.stomp_description" : "STOMP is a simple text-orientated messaging protocol. It defines " +
+    "an interoperable wire format so that any of the available STOMP clients can communicate with any STOMP " +
+    "message broker to provide easy and widespread messaging interoperability among languages and platforms",
+    "skills.web.libraries.jquery" : "jQuery",
+    "skills.web.libraries.jquery_description" : "jQuery is a cross-platform JavaScript library designed to simplify " +
+    "the client-side scripting of HTML.",
     "skills.web.libraries.chartjs" : "ChartJS",
+    "skills.web.libraries.chartjs_description" : "Simple, clean and engaging HTML5 based JavaScript charts. " +
+    "Chart.js is an easy way to include animated, interactive graphs on your website for free.",
     "skills.web.libraries.normalizr" : "normalizr",
+    "skills.web.libraries.normalizr_description" : "Normalizr is a small, but powerful utility for taking JSON with " +
+    "a schema definition and returning nested entities with their IDs, gathered in dictionaries.",
     "skills.web.project-tools" : "Web Project Tools",
-    "skills.web.project-tools.npm" : "Node Package Manager (npm)",
+    "skills.web.project-tools.npm" : "npm",
+    "skills.web.project-tools.npm_description" : "npm is a package manager for the JavaScript programming language. " +
+    "It is the default package manager for the JavaScript runtime environment Node.js.",
     "skills.web.project-tools.yarn" : "Yarn",
+    "skills.web.project-tools.yarn_description" : "Yarn is a fast, reliable, and secure dependency management.",
 
     "skills.android" : "Android Development",
+    "skills.android_description" : "Android is a mobile operating system developed by Google, based on the Linux " +
+    "kernel and designed primarily for touchscreen mobile devices such as smartphones and tablets.",
     "skills.android_comment" : "The Android development is my hobby. I have one successful project on Google Play.",
     "skills.android.dagger" : "Dagger 2",
+    "skills.android.dagger_description" : "Dagger is a fully static, compile-time dependency injection framework " +
+    "for both Java and Android.",
     "skills.android.arch-components" : "Android Architecture Components",
+    "skills.android.arch-components_description" : "Android Architecture Components - a collection of libraries " +
+    "that help you design robust, testable, and maintainable apps. Start with classes for managing your UI component " +
+    "lifecycle and handling data persistence.",
     "skills.android.data-binding" : "Data Binding Library",
+    "skills.android.data-binding_description" : "Data Binding Library generates a Binding class based on this layout. " +
+    "This class holds all the bindings from the layout properties.",
 
     "skills.c-sharp" : "C# Development",
+    "skills.c-sharp_description" : "C# - C# is a multi-paradigm programming language encompassing strong typing, " +
+    "imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.",
     "skills.c-sharp_comment" : "I've used C# for development while studying at university. C# LINQ inspired me to learn " +
     "Java Stream API and Java Lambda expressions.",
 
     "skills.databases" : "Databases",
     "skills.databases.sql" : "SQL",
+    "skills.databases.sql_description" : "SQL - a domain-specific language used in programming and designed for " +
+    "managing data held in a relational database management system, or for stream processing in a relational data " +
+    "stream management system.",
     "skills.databases.sqlite" : "SQLite",
+    "skills.databases.sqlite_description" : "SQLite is a self-contained, high-reliability, embedded, full-featured, " +
+    "public-domain, SQL database engine. SQLite is the most used database engine in the world.",
     "skills.databases.postgresql" : "PostgreSQL",
+    "skills.databases.postgresql_description" : "PostgreSQL is an object-relational database management system with " +
+    "an emphasis on extensibility and standards compliance.",
     "skills.databases.mysql" : "MySQL",
+    "skills.databases.mysql_description" : "MySQL is an open-source relational database management system.",
 
     "skills.vcs" : "Version Control Systems",
     "skills.vcs.git" : "Git",
-    "skills.vcs.svn" : "SVN",
+    "skills.vcs.git_description" : "Git is a version control system for tracking changes in computer files and " +
+    "coordinating work on those files among multiple people.",
+    "skills.vcs.svn" : "Apache Subversion",
+    "skills.vcs.svn_description" : "Apache Subversion is a software versioning and revision control system distributed as open " +
+    "source under the Apache License.",
 
     "skills.os" : "Software Operating Systems",
     "skills.os_comment" : "Linux: experienced user, bash working. Windows: experienced user, CMD working",
     "skills.os.linux" : "Linux",
-    "skills.os.windows" : "Windows",
+    "skills.os.linux_description" : "Linux is a name which broadly denotes a family of free and open-source software " +
+    "operating systems built around the Linux kernel. Typically, Linux is packaged in a form known as a " +
+    "Linux distribution for both desktop and server use.",
+    "skills.os.windows_description" : "Microsoft Windows is a metafamily of graphical operating systems developed, " +
+    "marketed, and sold by Microsoft.",
 
     "skills.scada" : "SCADA Development",
+    "skills.scada_description" : "Supervisory control and data acquisition (SCADA) is a control system architecture " +
+    "that uses computers, networked data communications and graphical user interfaces for high-level process " +
+    "supervisory management.",
     "skills.scada_comment" : "I've used these technologies when I've developed software for the blood centre automation systems.",
     "skills.scada.ms3" : "Master SCADA 3.x",
+    "skills.scada.ms3_description" : "SCADA system developed by InSat company.",
     "skills.scada.ms4" : "Master SCADA 4D",
+    "skills.scada.ms4_description" : "SCADA system developed by InSat company.",
     "skills.scada.universal-opc" : "Universal Master OPC Server",
+    "skills.scada.universal-opc_description" : "OPC = OLE for Process Control. OPC is a software interface standard " +
+    "that allows Windows programs to communicate with industrial hardware devices. OPC is implemented in " +
+    "server/client pairs. The OPC server is a software program that converts the hardware communication " +
+    "protocol used by a PLC into the OPC protocol.",
 
-    "skills.other" : "C/C++, Assembler, Perl etc.",
+    "skills.other" : "C/C++, Assembly language, Perl etc.",
     "skills.other.cpp" : "C/C++",
+    "skills.other.cpp_description" : "C++ is a general-purpose programming language. It has imperative, " +
+    "object-oriented and generic programming features, while also providing facilities for low-level memory manipulation.",
     "skills.other.perl" : "Perl",
-    "skills.other.assembler" : "Assembler"
+    "skills.other.perl_description" : "Perl is a family of high-level, general-purpose, interpreted, dynamic programming languages.",
+    "skills.other.assembler" : "Assembly language",
+    "skills.other.assembler_description" : "Assembly language - an assembly language, often abbreviated asm, is a low-level " +
+    "programming language for a computer, or other programmable device, in which there is a very strong correspondence " +
+    "between the language and the architecture's machine code instructions."
 };
 
 export default messages;
