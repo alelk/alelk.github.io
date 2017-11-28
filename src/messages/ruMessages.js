135cda1b3419cde9d7f5d21e5b6c3b22728c34b9
Alex Elkin <elckinne@gmail.com>
2017-11-28 09:25:48 +0300

    New skill descriptions have been added.

--- src/messages/ruMessages.js
+++ src/messages/ruMessages.js
@@ -53,6 +53,14 @@ const messages = {
     "skills.java.libraries.freemarker_description" : "FreeMarker - простой и легковесный шаблонный движок.",
     "skills.java.libraries.jackson" : "Jackson",
     "skills.java.libraries.jackson_description" : "Jackson - высокопроизводительный JSON обработчик в Java",
+    "skills.java.app-servers" : "Контейнеры сервлетов",
+    "skills.java.app-servers.wildfly" : "WildFly",
+    "skills.java.app-servers.wildfly_description" : "Java EE-сервер приложений с открытым исходным кодом, " +
+    "разработанный компанией JBoss.",
+    "skills.java.app-servers.tomcat" : "Apache Tomcat",
+    "skills.java.app-servers.tomcat_description" : "Apache Tomcat - контейнер сервлетов с открытым исходным кодом, " +
+    "разрабатываемый Apache Software Foundation. Реализует спецификацию сервлетов и спецификацию JavaServer " +
+    "Pages (JSP) и JavaServer Faces (JSF). Написан на языке Java.",
     "skills.java.build-tools" : "Системы сборки проектов",
     "skills.java.build-tools_comment" : "Во всех своих проектах использую Maven. С Gradle знаком благодаря Android.",
     "skills.java.build-tools.maven" : "Maven",
@@ -79,36 +87,77 @@ const messages = {
     "До знакомства с данными фреймворками, моим избранным инструментом создания веб-интерфейса был Java Thymeleaf. " +
     "Теперь же всю front-end работу с удовольствием выполняю, используя вышеупомянутые фреймворки.",
     "skills.web.react" : "React",
+    "skills.web.react_description" : "React — библиотека для разработки интерфейсов, поддерживаемая Facebook, " +
+    "Instagram. В React используется так называемый компонентный подход.",
     "skills.web.redux" : "Redux",
+    "skills.web.redux_description" : "Redux — инструмент управления как состоянием данных, так и состоянием " +
+    "интерфейса в JavaScript-приложениях.",
     "skills.web.html" : "HTML",
+    "skills.web.html_description" : "HTML (HyperText Markup Language) - язык гипертекстовой разметки.",
     "skills.web.css" : "CSS",
+    "skills.web.css_description" : "CSS (Cascading Style Sheets) — формальный язык описания внешнего вида документа, " +
+    "написанного с использованием языка разметки.",
     "skills.web.js" : "JavaScript",
+    "skills.web.js_description" : "JavaScript – язык программирования, с помощью которого веб-страницам придается " +
+    "интерактивность. С его помощью создаются приложения, которые включаются в HTML-код.",
     "skills.web.es6" : "ES6",
+    "skills.web.es6_description" : "ES6 - значительное обновление языка ECMAScript (JavaScript)",
     "skills.web.libraries": "Библиотеки и фреймворки",
     "skills.web.libraries.rxjs" : "RxJS",
+    "skills.web.libraries.rxjs_description" : "RxJS - реактивное расширение для фронтенд разработки.",
     "skills.web.libraries.semantic-ui" : "Semantic UI React",
+    "skills.web.libraries.semantic-ui_description" : "Semantic UI React - фреймворк разработки пользовательского " +
+    "интерфейса на React",
     "skills.web.libraries.material-ui" : "Material-UI",
+    "skills.web.libraries.material-ui_description" : "Реализация концепции Material Design от Google для React",
     "skills.web.libraries.redux-thunk" : "Redux Thunk",
+    "skills.web.libraries.redux-thunk_description" : "Redux Thunk - мидлвэр (middleware) управления асинхронными действиями в Redux",
     "skills.web.libraries.react-router" : "React Router",
+    "skills.web.libraries.react-router_description" : "React Router - библиотека URL-маршрутизации на стороне клиента " +
+    "для React",
     "skills.web.libraries.react-intl" : "React Intl",
+    "skills.web.libraries.react-intl_description" : "React Intl - библиотека интернационализации React-приложений. " +
+    "Поддерживает плюрализацию, форматирование дат, чисел строк.",
     "skills.web.libraries.react-table" : "React Table",
+    "skills.web.libraries.react-table_description" : "React Table - мощная библиотека построения таблиц на React. " +
+    "Поддерживает пагинацию, фильтрацию, сортировку данных как на стороне клиента, так и на стороне сервера.",
     "skills.web.libraries.sockjs" : "SockJS",
-    "skills.web.libraries.stomp" : "STOMP",
-    "skills.web.libraries.jquery" : "JQuery",
+    "skills.web.libraries.sockjs_description" : "SockJS - клиентская библиотека написанная на javascript, " +
+    "которая имитирует API веб-сокетов, но при этом поддерживает все браузеры путем использования различных " +
+    "суррогатных заменителей в виде ajax long-polling, jsonp-polling и т.д.",
+    "skills.web.libraries.stomp" : "STOMP JS",
+    "skills.web.libraries.stomp_description" : "STOMP — простой протокол обмена сообщениями.",
+    "skills.web.libraries.jquery" : "jQuery",
+    "skills.web.libraries.jquery_description" : "jQuery — библиотека JavaScript, фокусирующаяся на взаимодействии " +
+    "JavaScript и HTML. Библиотека jQuery помогает легко получать доступ к любому элементу DOM, обращаться к " +
+    "атрибутам и содержимому элементов DOM, манипулировать ими.",
     "skills.web.libraries.chartjs" : "ChartJS",
+    "skills.web.libraries.chartjs_description" : "ChartJS - инструмент, предназначенный для создания графиков и диаграмм.",
     "skills.web.libraries.normalizr" : "normalizr",
+    "skills.web.libraries.normalizr_description" : "normalizr - утилита, позволяющая парсить JSON-данные по шаблону: " +
+    "выделать категории данных, находить ключи, производить предварительную обработку и т.д.",
     "skills.web.project-tools" : "Системы управления проектами",
-    "skills.web.project-tools.npm" : "Node Package Manager (npm)",
+    "skills.web.project-tools.npm" : "npm",
+    "skills.web.project-tools.npm_description" : "npm - менеджер пакетов.",
     "skills.web.project-tools.yarn" : "Yarn",
+    "skills.web.project-tools.yarn_description" : "Yarn - новый менеджер пакетов, созданный Facebook, Google, " +
+    "Exponent, Tilde, решающий некоторые проблемы с безопасностью, существующие в npm.",
 
     "skills.android" : "Разработка для Android",
     "skills.android_comment" : "Разработкой для Android занимаюсь в свободное время. Успешно реализовал и опубликовал " +
     "в Google Play один собственный проект.",
+    "skills.android_description" : "Android - операционная система для смартфонов, планшетов, электронных книг, " +
+    "цифровых проигрывателей, наручных часов, игровых приставок, нетбуков, смартбуков, очков Google, телевизоров и " +
+    "других устройств. Основана на ядре Linux и собственной реализации виртуальной машины Java от Google.",
     "skills.android.dagger" : "Dagger 2",
+    "skills.android.dagger_description" : "Dagger - инструмент внедрения зависимостей (DI, dependency injection) во время компиляции.",
     "skills.android.arch-components" : "Android Architecture Components",
     "skills.android.data-binding" : "Data Binding Library",
+    "skills.android.data-binding_description" : "Data Binding Library - ряд вспомогательных библиотек, которые призваны помочь " +
+    "с проектированием, тестированием и сопровождением приложений.",
 
     "skills.c-sharp" : "Разработка на C#",
+    "skills.c-sharp_description" : "C# - объектно-ориентированный язык программирования со строгой типизацией.",
     "skills.c-sharp_comment" : "Давно, еще до моего знакомства с Java, все свои проекты я писал на C#. " +
     "В основном это были различные университетские проекты. С тех пор у меня сохранилось дружественное " +
     "отношение к C#, стараюсь не потерять опыт программирования на этом языке. Время от времени " +
@@ -116,34 +165,55 @@ const messages = {
 
     "skills.databases" : "Базы данных и SQL",
     "skills.databases.sql" : "SQL",
+    "skills.databases.sql_description" : "SQL (structured query language) — язык программирования, применяемый " +
+    "для создания, модификации и управления данными в реляционной базе данных, управляемой соответствующей системой " +
+    "управления базами данных.",
     "skills.databases.sqlite" : "SQLite",
+    "skills.databases.sqlite_description" : "SQLite - встраиваемая кроссплатформенная БД, которая поддерживает " +
+    "достаточно полный набор команд SQL и доступна в исходных кодах (на языке C).",
     "skills.databases.postgresql" : "PostgreSQL",
+    "skills.databases.postgresql_description" : "PostgreSQL - свободная объектно-реляционная система управления базами данных (СУБД).",
     "skills.databases.mysql" : "MySQL",
+    "skills.databases.mysql_description" : "MySQL - одна из самых популярных и самых распространенных СУБД в интернете.",
 
     "skills.vcs" : "Системы контроля версий",
     "skills.vcs.git" : "Git",
-    "skills.vcs.svn" : "SVN",
+    "skills.vcs.git_description" : "Git - распределённая система управления версиями.",
+    "skills.vcs.svn" : "Apache Subversion",
+    "skills.vcs.svn_description" : "Apache Subversion — свободная централизованная система управления версиями.",
 
     "skills.os" : "Операционные системы",
     "skills.os_comment" : "Linux: работа в качестве опытного пользователя, работа в bash, написание простых скриптов. " +
     "Windows: опытный пользователь, работа с CMD",
     "skills.os.linux" : "Linux",
+    "skills.os.linux_description" : "Linux - общее название UNIX-подобных операционных систем, созданных на основе свободного ядра.",
     "skills.os.windows" : "Windows",
+    "skills.os.windows_description" : "Windows - семейство проприетарных операционных систем корпорации Microsoft.",
 
     "skills.scada" : "Разработка SCADA-систем",
+    "skills.scada_description" : "SCADA (Supervisory Control And Data Acquisition) — программный пакет, " +
+    "предназначенный для разработки или обеспечения работы в реальном времени систем сбора, обработки, отображения " +
+    "и архивирования информации об объекте мониторинга или управления.",
     "skills.scada_comment" : "При разработке мной ПО мониторинга и автоматизации производственных процессов для " +
     "нижегородского центра крови, часть этих задач была реализована путем разработки SCADAсистем. В ходе данной " +
     "работы был приобретен опыт: Конфигурирование OPC-сервера: Опрос OPC-переменных по протоколу Modbus " +
     "или произвольному протоколу. Написание скриптов на языке Lua. Разработка ПО в Master SCADA 3.x: " +
     "Проектирование сложных мнемосхем, работа с трендами, отчетами, архивными данными, написание скриптов на языке C#",
     "skills.scada.ms3" : "Master SCADA 3.x",
+    "skills.scada.ms3_description" : "SCADA-система, разработанная компанией InSat.",
     "skills.scada.ms4" : "Master SCADA 4D",
+    "skills.scada.ms4_description" : "SCADA-система, разработанная компанией InSat.",
     "skills.scada.universal-opc" : "Universal Master OPC Server",
+    "skills.scada.universal-opc_description" : "OPC (Open Platform Communications) — семейство программных " +
+    "технологий, предоставляющих единый интерфейс для управления объектами автоматизации и технологическими процессами.",
 
-    "skills.other" : "C/C++, Assembler, Perl и т.д.",
+    "skills.other" : "C/C++, язык ассемблера, Perl и т.д.",
     "skills.other.cpp" : "C/C++",
+    "skills.other.cpp_description" : "C++ — компилируемый, статически типизированный язык программирования общего назначения.",
     "skills.other.perl" : "Perl",
-    "skills.other.assembler" : "Assembler"
+    "skills.other.perl_description" : "Perl - высокоуровневый интерпретируемый динамический язык программирования.",
+    "skills.other.assembler" : "Язык ассемблера",
+    "skills.other.assembler_description" : "Язык ассемблера (assembly language) — машинно-ориентированный язык низкого уровня."
 };
 
 export default messages;
